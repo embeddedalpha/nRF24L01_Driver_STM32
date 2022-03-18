e878326decc99e6e6fc3b4f9990a4ecc1b6e9533
Kunal Salvi <kunalsalvi63@gmail.com>
2022-03-18 23:55:44 +0530

    First Commit

---
+++ Debug/Src/main.d
@@ -0,0 +1,31 @@
+Src/main.o: ../Src/main.c ../Inc/main.h ../Inc/stm32f4xx.h \
+ ../Inc/stm32f411xe.h ../Inc/core_cm4.h ../Inc/cmsis_version.h \
+ ../Inc/cmsis_compiler.h ../Inc/cmsis_gcc.h ../Inc/mpu_armv7.h \
+ ../Inc/system_stm32f4xx.h ../Inc/Console/Console.h ../Inc/GPIO/GPIO.h \
+ ../Inc/Console/Console.h ../Inc/SPI/SPI.h
+
+../Inc/main.h:
+
+../Inc/stm32f4xx.h:
+
+../Inc/stm32f411xe.h:
+
+../Inc/core_cm4.h:
+
+../Inc/cmsis_version.h:
+
+../Inc/cmsis_compiler.h:
+
+../Inc/cmsis_gcc.h:
+
+../Inc/mpu_armv7.h:
+
+../Inc/system_stm32f4xx.h:
+
+../Inc/Console/Console.h:
+
+../Inc/GPIO/GPIO.h:
+
+../Inc/Console/Console.h:
+
+../Inc/SPI/SPI.h:
