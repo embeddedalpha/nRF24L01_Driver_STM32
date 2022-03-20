fc4ed3a826f41e26c38883566e3593b66c72346b
Kunal Salvi <kunalsalvi63@gmail.com>
2022-03-20 13:02:14 +0530

    Updated main.c. To be used as example.

--- Src/main.c
+++ Src/main.c
@@ -21,11 +21,9 @@
 #include "GPIO/GPIO.h"
 #include "Console/Console.h"
 #include "SPI/SPI.h"
+#include "nRF24L01/nRF24L01_Reg.h"
 
 
-//#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-//  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-//#endif
 
 int main(void)
 {
@@ -34,3 +32,4 @@ int main(void)
 
 	for(;;);
 }
+
